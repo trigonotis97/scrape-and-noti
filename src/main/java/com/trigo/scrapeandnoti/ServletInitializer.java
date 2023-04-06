5a65b2859ebe284ff355e5065d546e1ce4c6c140
cheonhwa kim <trigonotis97@gmail.com>
2023-04-06 15:01:34 +0900

    init project

---
+++ src/main/java/com/trigo/scrapeandnoti/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.trigo.scrapeandnoti;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Main.class);
+    }
+
+}
---
+++ src/test/java/com/trigo/scrapeandnoti/ScrapeAndNotiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trigo.scrapeandnoti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrapeAndNotiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
